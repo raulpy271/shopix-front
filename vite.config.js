b5fe390822f807fab83212dbe79bfdcf3ed8934a
raulpy271 <raulrufino0@gmail.com>
2025-04-12 15:26:57 -0300

    Adiciona biblioteca axios

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+      '@Components': path.resolve('./src/Components'),
+      '@Controllers': path.resolve('./src/Controllers'),
+      '@Layouts': path.resolve('./src/Layouts'),
+      '@Pages': path.resolve('./src/Pages'),
+      '@Theme': path.resolve('./src/Theme'),
+    }
+  }
 })
