ebed67671cf4f0c3896b406984516e9690c451c2
raulpy271 <raulrufino0@gmail.com>
2025-05-10 16:30:50 -0300

    Adiciona página not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import Pages from './Pages'
+import NotFound from './Pages/NotFound';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         {
           Pages.map((page, index) => <Route key={index} path={page.path} element={<page.component/>}/>)
         }
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import Template from '../../Layouts/Template';
+
+export default function NotFound() {
+  return (
+    <>
+      <Template/>
+      <div className="w-full flex align-items justify-center">
+        <img className="w-1/4 transition delay-100 duration-300 ease-in-out hover:invert" src="/src/assets/not-found.jpg"></img>
+      </div>
+    </>
+  )
+}
